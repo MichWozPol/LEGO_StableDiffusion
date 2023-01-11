e10c869c48b54cb43d18526ec8448e594b68d6b3
michsak <michalsakowicz99@gmail.com>
2023-01-11 19:18:09 +0000

    add app basic structure

--- app/main.py
+++ app/main.py
@@ -1,12 +1,28 @@
 import gradio as gr
+import numpy as np
+import time
 
-def greet(name):
-    return "Hello " + name + "!"
 
-with gr.Blocks() as demo:
-    name = gr.Textbox(label="Name")
-    output = gr.Textbox(label="Output Box")
-    greet_btn = gr.Button("Greet")
-    greet_btn.click(fn=greet, inputs=name, outputs=output)
+def fake_diffusion(input_val):  
+    print(input_val)
+    for _ in range(1,100):
+        time.sleep(1)
+        image = np.random.random((600, 600, 3))
+        yield image
+    image = "https://i.picsum.photos/id/867/600/600.jpg?hmac=qE7QFJwLmlE_WKI7zMH6SgH5iY5fx8ec6ZJQBwKRT44" 
+    yield image
 
-demo.launch()
+
+with gr.Blocks() as app:
+    gr.Markdown("<h2>Generate your OWN image in LEGO style</h2>")
+
+    text_input = gr.Textbox()
+    model_output = gr.Image()
+    text_button = gr.Button("Generate")
+
+    text_button.click(fake_diffusion, inputs=text_input, outputs=model_output)
+    
+
+if __name__ == "__main__":
+    app.queue()
+    app.launch()  
