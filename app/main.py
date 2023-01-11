6f7cc6969fab4ee578ca051934c0d8052c3f90ce
michsak <michalsakowicz99@gmail.com>
2023-01-11 19:06:50 +0100

    initiate app

---
+++ app/main.py
@@ -0,0 +1,12 @@
+import gradio as gr
+
+def greet(name):
+    return "Hello " + name + "!"
+
+with gr.Blocks() as demo:
+    name = gr.Textbox(label="Name")
+    output = gr.Textbox(label="Output Box")
+    greet_btn = gr.Button("Greet")
+    greet_btn.click(fn=greet, inputs=name, outputs=output)
+
+demo.launch()
