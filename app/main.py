8d93ac08c71a0858d8c2bd0dfe3aa079b9ea9f5f
michsak <michalsakowicz99@gmail.com>
2023-01-12 11:27:05 +0000

    correct relative paths

--- app/main.py
+++ app/main.py
@@ -3,12 +3,12 @@ import numpy as np
 import time
 import torch
 import os
-os.environ["CUDA_VISIBLE_DEVICES"] = "1" #choose GPU index
+os.environ["CUDA_VISIBLE_DEVICES"] = "0" #choose GPU index
 from diffusers import StableDiffusionPipeline
 
 
 model_names = ["Classic model 1", "Classic model 2", "Figure model"]
-models = {model_names[0]: ".model_1_30", model_names[1]: ".model_1_60", model_names[2]: ".model_2_30"}
+models = {model_names[0]: "./app/.model_1_30", model_names[1]: "./app/.model_1_60", model_names[2]: "./app/.model_2_30"}
 current_model = models[model_names[0]]
 disable_safety = False
 
@@ -33,8 +33,9 @@ def null_safety(images, **kwargs):
       return images, False
           
 
-with gr.Blocks(css="main.css") as app:
-    gr.Markdown("<h2>Generate your OWN image in LEGO style</h2>")    
+with gr.Blocks(css="./app/main.css") as app:
+    gr.Markdown("<h2>Generate your OWN image in LEGO style</h2>")  
+      
     text_input = gr.Textbox(label="Prompt")
     switch_model = gr.Radio(label="Choose Model", choices=model_names, value=model_names[0]).style(item_container=True, container=True)
     model_output = gr.Image(label="Output Image", elem_id="out_img")
